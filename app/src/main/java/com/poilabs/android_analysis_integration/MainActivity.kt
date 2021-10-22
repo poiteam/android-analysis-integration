b38d8135cffaf75a89267d7e92366facc7d2af18
Mohammad Khakpaki <mohammad@poilabs.com>
2021-10-22 18:18:10 +0300

    Implement SDK

--- app/src/main/java/com/poilabs/android_analysis_integration/MainActivity.kt
+++ app/src/main/java/com/poilabs/android_analysis_integration/MainActivity.kt
@@ -1,11 +1,101 @@
 package com.poilabs.android_analysis_integration
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import getpoi.com.poibeaconsdk.PoiAnalysis
+import getpoi.com.poibeaconsdk.models.PoiResponseCallback
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PoiResponseCallback {
+    companion object {
+        private const val REQUEST_FOREGROUND_LOCATION_REQUEST_CODE = 56
+        private const val REQUEST_BACKGROUND_LOCATION_REQUEST_CODE = 57
+        private const val REQUEST_COARSE_LOCATION = 58
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askRuntimePermissionsIfNeeded()
+    }
+
+    private fun askRuntimePermissionsIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val hasFineLocation: Int =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            val hasBackgroundLocation: Int = ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+            if (hasFineLocation != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    REQUEST_FOREGROUND_LOCATION_REQUEST_CODE
+                )
+            }
+            if (hasBackgroundLocation != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ),
+                    REQUEST_BACKGROUND_LOCATION_REQUEST_CODE
+                )
+            }
+        } else {
+            val hasLocalPermission: Int =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            if (hasLocalPermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    REQUEST_COARSE_LOCATION
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+      if (requestCode == REQUEST_COARSE_LOCATION) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults[0]) { // Permission Granted
+             startPoiSdk()
+            }
+        } else if (requestCode == REQUEST_BACKGROUND_LOCATION_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults[0]) { // Permission Granted
+                startPoiSdk()
+            }
+        }
+    }
+
+
+    private fun startPoiSdk() {
+        PoiAnalysis.getInstance().enable()
+        PoiAnalysis.getInstance().startScan(applicationContext)
+        PoiAnalysis.getInstance().setPoiResponseListener(this)
+    }
+
+    override fun onResponse(p0: String?) {
+        Log.i(TAG, "onResponse: $p0")
+    }
+
+    override fun onFail(p0: Exception?) {
+        Log.e(TAG, "onFail: ${p0?.localizedMessage}")
+        p0?.printStackTrace()
     }
 }
+
---
+++ app/src/main/java/com/poilabs/android_analysis_integration/MyApplication.kt
@@ -0,0 +1,28 @@
+package com.poilabs.android_analysis_integration
+
+import android.app.Application
+import android.content.Intent
+import getpoi.com.poibeaconsdk.PoiAnalysis
+import getpoi.com.poibeaconsdk.models.PoiAnalysisConfig
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val config = PoiAnalysisConfig(BuildConfig.APPSECRET, "this is a test unique id", BuildConfig.APPID)
+        config.setOpenSystemBluetooth(true)
+        config.setForegroundServiceIntent(Intent(this, MainActivity::class.java))
+        config.enableForegroundService()
+        config.setServiceNotificationTitle("Searching for campaigns...")
+        config.setForegroundServiceNotificationChannelProperties(
+            "My Notification Name",
+            "My Notification Channel Description"
+        )
+        PoiAnalysis.getInstance(this, config)
+
+        PoiAnalysis.getInstance().enable()
+
+    }
+
+}
